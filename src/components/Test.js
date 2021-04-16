30c78bd86345a6090f745c2058165ae870d74478
Jack Funck <jackfunck21@gmail.com>
2021-04-16 10:09:51 -0400

    working my way up in React

---
+++ src/components/Test.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Test () {
+    return(
+        <div>
+        <h1>This is a test</h1>
+        <p>Hopfully this works</p>
+        </div>
+    );
+
+}
+
+export default Test;
